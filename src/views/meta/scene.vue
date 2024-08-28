533b2a03ff4e75714df99d4b9d4ab0477abe37ea
gdgeek <dirui1@yeah.net>
2024-08-28 18:16:28 +0800

    no message

--- src/views/meta/scene.vue
+++ src/views/meta/scene.vue
@@ -89,7 +89,7 @@ const handleMessage = async (e: MessageEvent) => {
   const action = e.data.action;
   const data = e.data.data;
   //const data = e.data;
-  console.error(3);
+  alert(action);
   switch (action) {
     case "save":
       saveMeta(data);
