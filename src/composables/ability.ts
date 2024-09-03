bce6358c7d4f6ade961bea62db35faeccf08bca7
gdgeek <dirui1@yeah.net>
2024-09-03 15:06:05 +0800

    权限管理

--- src/composables/ability.ts
+++ src/composables/ability.ts
@@ -1,12 +1,7 @@
-import { MongoAbility } from "@casl/ability";
+// src/ability.ts
+import { defineAbility } from '@casl/ability';
 
-export const useAbility = () => {
-  const $ability = ref<MongoAbility | null>(null);
-
-  const can = (action: string, subject: any, fields?: any) => {
-    if (!$ability.value) return false;
-    return $ability.value.can(action, subject, fields);
-  };
-
-  return { can };
-};
+export const ability = defineAbility((can, cannot) => {
+  can('read', 'Article'); // 允许读取 Article
+  can('create', 'Article', { authorId: 1 }); // 只有 authorId 为 1 的用户可以创建 Article
+});
