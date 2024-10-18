7b2241929163591e3be7caba3ccbfa2c2eee1ab4
huyuelong <949910047@qq.com>
2024-10-18 14:53:38 +0800

    feat:

--- src/utils/silent_request.ts
+++ src/utils/silent_request.ts
@@ -1,6 +1,39 @@
 import axios, { InternalAxiosRequestConfig, AxiosResponse } from "axios";
 import { TOKEN_KEY } from "@/enums/CacheEnum";
-import env from "@/environment";
+import i18n from "@/lang";
+
+// 获取当前语言
+const lang = ref(i18n.global.locale.value);
+watch(
+  () => i18n.global.locale.value,
+  (newLang) => {
+    lang.value = newLang;
+  }
+);
+
+// 动态错误消息
+const messages = {
+  en: [
+    "Login expired, please log in again",
+    "Network error, please check your internet connection",
+    "Internal server error, please try again later",
+  ],
+  zh: [
+    "登录过期，请重新登录",
+    "网络错误，请检查您的网络连接",
+    "服务器内部错误，请稍后再试",
+  ],
+};
+
+const getMessageArray = () => {
+  switch (lang.value) {
+    case "en":
+      return messages.en;
+    case "zh-cn":
+    default:
+      return messages.zh;
+  }
+};
 
 // 创建 axios 实例
 const service = axios.create({
@@ -25,15 +58,55 @@ service.interceptors.request.use(
   }
 );
 
+// 显示错误消息
+function showErrorMessage(message: string, duration = 5000) {
+  ElMessage({
+    message,
+    type: "error",
+    duration,
+  });
+}
+
+// 处理身份认证失败
+function handleUnauthorized(router: ReturnType<typeof useRouter>) {
+  const messages = getMessageArray();
+  showErrorMessage(messages[0]);
+  router.push({ path: "/login" });
+  return Promise.reject("");
+}
+
 // 响应拦截器
 service.interceptors.response.use(
   (response: AxiosResponse) => {
     return response;
   },
   (error: any) => {
-    return Promise.reject(error.response.data);
+    const router = useRouter();
+    const { response } = error;
+    const messages = getMessageArray();
+
+    if (!response) {
+      if (error.message === "Network Error") {
+        showErrorMessage(messages[1]);
+      } else {
+        showErrorMessage(error.message);
+      }
+      return Promise.reject(error);
+    }
+
+    if (response.status === 401) {
+      // 处理 401 身份认证失败
+      return handleUnauthorized(router);
+    } else if (response.status >= 500) {
+      // 处理服务器错误
+      showErrorMessage(messages[2]);
+    } else {
+      const message = response.data.message || error.message;
+      showErrorMessage(message);
+    }
+
+    return Promise.reject(response);
   }
 );
 
-// 导出 axios 实例
 export default service;
