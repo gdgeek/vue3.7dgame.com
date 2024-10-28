d4cfffc35a451ee1c1a38316ff4e2a4f52d92690
huyuelong <949910047@qq.com>
2024-10-28 09:55:52 +0800

    feat:

--- src/store/modules/useHoverStore.ts
+++
@@ -1,9 +0,0 @@
-export const useHoverStore = defineStore("hover", () => {
-  const hovering = ref(false);
-
-  const setHovering = (value: boolean) => {
-    hovering.value = value;
-  };
-
-  return { hovering, setHovering };
-});
