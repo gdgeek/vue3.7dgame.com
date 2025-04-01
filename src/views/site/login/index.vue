7871f77cd49fe3ab46754cdcf44ed8b765ed69b4
dirui <dirui1@yeah.net>
2025-04-01 10:46:12 +0800

    no message

--- src/views/site/login/index.vue
+++ src/views/site/login/index.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <div v-loading="loading" :class="['box1', { 'dark-theme': isDark }]">
       <div :class="['box2', { 'dark-theme': isDark }]">
-        <h1>{{ $t("login.h1") }}~</h1>
+        <h1>{{ $t("login.h1") }}</h1>
 
         <h4>{{ $t("login.h4") }}</h4>
         <br />
@@ -31,7 +31,7 @@ import Wechat from "@/components/Account/Wechat.vue";
 const settingsStore = useSettingsStore();
 const isDark = computed<boolean>(() => settingsStore.theme === ThemeEnum.DARK);
 const loading = ref<boolean>(false);
-alert(environment.api);
+
 </script>
 
 <style scoped lang="scss">
